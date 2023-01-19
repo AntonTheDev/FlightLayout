766202211155e940063f9c7ba630eaccc3da0897
Anton <anton.doudarev@gmail.com>
2023-01-19 17:11:55 -0600

    updated some code to try swift package manager

---
+++ Tests/FlightLayoutTests/FlightLayoutTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FlightLayout
+
+final class FlightLayoutTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FlightLayout().text, "Hello, World!")
+    }
+}
